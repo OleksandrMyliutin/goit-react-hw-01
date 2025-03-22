233ad66f16ae25e48c1a7ae041c5d70196139d95
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-03-22 16:43:06 +0200

    remove css calss

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,8 +4,8 @@ const FriendList = ({avatar, name, isOnline }) => {
     return (
         <li className={s.item}>
             <img className={s.avatar} src={avatar} alt="Avatar" width="48" />
-            <h3 className={s.name}>{name}</h3>
-            <p >{isOnline ? <h4 className={clsx(s.isOnline)}>Online</h4>:<h4 className={clsx(s.isOffline)}> Offline</h4>}</p>
+            <h2 className={s.name}>{name}</h2>
+            <p >{isOnline ? <h4 className={clsx(s.status, s.isOnline)}>Online</h4>:<h4 className={clsx(s.status, s.isOffline)}> Offline</h4>}</p>
         </li>
     );
 };
